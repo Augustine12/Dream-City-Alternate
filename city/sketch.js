dd479c784d3a2832528382245c3166ec45db79a9
Augustine12 <wilsons@mx.lakeforest.edu>
2017-02-16 11:46:47 -0600

    Replicated image Dream City

---
+++ city/sketch.js
@@ -0,0 +1,80 @@
+function setup() {
+createCanvas(600, 600);
+}
+
+function draw() {
+background(255,64,93);
+    
+
+    beginShape();//shape 2 
+    fill(5,0,1);//very dark red 
+vertex(481.3,0);
+vertex(600,0);
+vertex(600,212.7);
+vertex(434.8,600);
+vertex(324.2,600);
+vertex(591.8,40.8);
+vertex(481.3,0);
+vertex(481.3,0);    
+endShape(CLOSE);
+    
+    
+   
+    
+    
+    beginShape();//shape 3
+    fill(64,255,226);//cyan 
+vertex(546.2,123.2);
+vertex(324.2,600);
+vertex(0,600);
+vertex(0,19.7);
+endShape(CLOSE);
+    
+    
+    
+     beginShape();//shape 4
+fill(255,131,64);//0range 
+vertex(0,18.8);
+vertex(402.3,86.4);
+vertex(98.8,600);
+vertex(0,600);
+endShape(CLOSE);
+    
+    
+     beginShape();//shape 5
+fill(64,255,226);//cyan 
+vertex(224.6,54.8);
+vertex(0.1,545.2);
+vertex(0.1,25);
+endShape(CLOSE);
+    
+     
+    
+    
+      
+ strokeWeight(1);//shape 6
+fill(255,64,189);//light pink  
+ beginShape();
+vertex(266.4,-1);
+vertex(0,275.7);
+vertex(0,2.1);
+endShape(CLOSE);
+    
+    
+    
+  beginShape();//shape 7
+fill(255,64,189);//light pink 
+vertex(598,212.7);
+vertex(432.8,600);
+vertex(598,600);
+endShape(CLOSE);
+    
+    beginShape();//shape 1
+  fill(255,64,93);//light red     
+vertex(600,381.1);
+vertex(508.5,600);
+vertex(600,600);
+endShape(CLOSE); 
+       
+}
+    
